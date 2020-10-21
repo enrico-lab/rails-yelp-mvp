b9d0c1239f4ce05d7976231527e1b752217b70ed
enrico favaro <enricofavaro@yahoo.it>
2020-10-21 16:10:08 +0200

    in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5, category: "italian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4, category: "indian" }
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.new(attributes)
+  p restaurant.valid?
+  p restaurant.errors.messages
+  restaurant.save!
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
